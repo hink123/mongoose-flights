1f473ca2dee7ce1a36cbd62356992f19580c5b59
Will Hinkelman <will.hink24@gmail.com>
2019-11-18 14:45:34 -0800

    flight destinations

---
+++ routes/arrivals.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+var arrivalsCtlr = require('../controllers/arrivals');
+
+router.post('/flights/:id/arrivals', arrivalsCtlr.create);
+
+module.exports = router;
