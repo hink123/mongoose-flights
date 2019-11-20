8c8ec05de469a2d859053496098e80d859b5a539
Will Hinkelman <will.hink24@gmail.com>
2019-11-20 10:37:01 -0800

    able to remove tickets

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -3,7 +3,8 @@ var Flight = require('../models/flight');
 
 module.exports = {
     new: newTicket,
-    create
+    create, 
+    delete: deleteTicket
 }
 
 function newTicket(req, res) {
@@ -16,4 +17,14 @@ function create(req, res) {
     Ticket.create(req.body, function(err) {
         res.redirect(`/flights/${req.body.flight}`)
     });
+}
+
+function deleteTicket(req, res) {
+    var flightId;
+    Ticket.findById(req.params.id, function(err, ticket) {
+        flightId = ticket.flight;
+    });
+    Ticket.deleteOne({_id: req.params.id}, function(err) {
+        res.redirect(`/flights/${flightId}`);
+  })
 }
--- routes/tickets.js
+++ routes/tickets.js
@@ -5,5 +5,6 @@ var ticketsCtlr = require('../controllers/tickets');
 
 router.get('/flights/:id/tickets/new', ticketsCtlr.new);
 router.post('/flights/:id/tickets', ticketsCtlr.create);
+router.delete('/tickets/:id', ticketsCtlr.delete);
 
 module.exports = router;
